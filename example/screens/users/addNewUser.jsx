1ad84ae09b97e62115b54a780e043fe6a3af2369
haslan82 <haslan82@hotmail.com>
2024-12-11 23:39:55 +0300

    ekledim

--- example/screens/users/addNewUser.jsx
+++ example/screens/users/addNewUser.jsx
@@ -1,33 +1,74 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import defaultScreenStyle from '../../styles/defaultScreenStyle';
 import Input from '../../components/ui/input';
 import Button from '../../components/ui/button';
 import {Formik} from 'formik';
 
+
 const AddNewUser = () => {
   return (
     <View style={defaultScreenStyle.container}>
       <Formik
         initialValues={{
-          name: '',
-          surname: '',
-          email: '',
-          phoneNumber: '',
-          gender: '',
-          age:'',
+          name: 'Hüseyin',
+          surname: 'ASLAN',
+          email: 'aslan@gmail.com',
+          phoneNumber:"7778899",
+          gender: 'erkek',
+          age: '33',
         }}
         onSubmit={values => console.log(values)}>
         {({handleChange, handleBlur, handleSubmit, values}) => (
-          <View>
-            <Input value={values.name} title="Name" placeholder="Please set name" />
-            <Input value={values.surname} title="Surname" placeholder="Please set surname" />
-            <Input value={values.email} title="E-mail" placeholder="Please set e-mail" />
-            <Input value={values.phoneNumber} title="Phone Number" placeholder="Please set phone number" />
-            <Input value={values.gender} title="Gender" placeholder="Please set your gender" />
-            <Input value={values.age} title="Age" placeholder="Please set your age" />
+          <ScrollView>
+            <Input
+              onChangeText={handleChange('name')}
+              onBlur={handleBlur('name')}
+              value={values.name}
+              title="Name"
+              placeholder="Please set name"
+            />
+            <Input
+              onChangeText={handleChange('surname')}
+              onBlur={handleBlur('surname')}
+              value={values.surname}
+              title="Surname"
+              placeholder="Please set surname"
+            />
+            <Input
+            keyboardType="emai-address" 
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+              title="E-mail"
+              placeholder="Please set e-mail"
+            />
+            <Input
+            /* keyboardType="number-pad" */
+            keyboardType="phone-pad" 
+              onChangeText={handleChange('phoneNumber')}
+              onBlur={handleBlur('phoneNumber')}
+              value={values.phoneNumber}
+              title="Phone Number"
+              placeholder="Please set phone number"
+            />
+            <Input
+              onChangeText={handleChange('gender')}
+              onBlur={handleBlur('gender')}
+              value={values.gender}
+              title="Gender"
+              placeholder="Please set your gender"
+            />
+            <Input
+            keyboardType="number-pad"
+              onChangeText={handleChange('age')}
+              onBlur={handleBlur('age')}
+              value={values.age}
+              title="Age"
+              placeholder="Please set your age"
+            />
             <Button onPress={handleSubmit} title="Save" status="success" />
-          </View>
+          </ScrollView>
         )}
       </Formik>
     </View>
---
+++ example/utils/validationSchema.js
@@ -0,0 +1,13 @@
+import * as Yup from 'yup';
+
+const newUserSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  surname: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+});
